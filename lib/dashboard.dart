485b236f3224edf0441ea657be7d79255d370b58
gichimux <gichimumwai1@gmail.com>
2021-04-20 20:55:40 +0300

    create dashboard grid

---
+++ lib/dashboard.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  Material myItems(IconData icon, String heading, int color) {
+    return Material(
+      color: Colors.white,
+      elevation: 14.0,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(22.0),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  
+                   Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(heading,
+                      style: TextStyle(
+                        color: new Color(color),
+                        fontSize: 20.0,
+                      ),
+                      ),
+                    ),
+                  
+                  // Icon
+                  Material(
+                    color: new Color(color),
+                    borderRadius: BorderRadius.circular(22.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Icon(
+                        icon,
+                      color: Colors.white,
+                      size: 30.0,
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+            ],
+          ),
+        ) ,),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'M-chama',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: StaggeredGridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        children: <Widget>[
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+          myItems(Icons.graphic_eq, "name", 0xffed22b),
+        ],
+        staggeredTiles: [
+          StaggeredTile.extent(2, 130),
+          StaggeredTile.extent(1, 150),
+          StaggeredTile.extent(1, 150),
+          StaggeredTile.extent(1, 150),
+          StaggeredTile.extent(1, 150),
+          StaggeredTile.extent(2, 130),
+        ],
+      ),
+    );
+  }
+}
