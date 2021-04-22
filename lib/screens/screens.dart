9575255e1fb01d47115849d31b006a1b4738104c
gichimux <gichimumwai1@gmail.com>
2021-04-22 23:45:11 +0300

    create bottom navigation bar

---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:chama_app_ui/screens/home_screen.dart';
+export 'package:chama_app_ui/screens/member/dashboard.dart';
+export 'package:chama_app_ui/screens/member/analysis.dart';
+export 'package:chama_app_ui/screens/admin/dashboard.dart';
+export 'package:chama_app_ui/screens/bottom_nav_screen.dart';
